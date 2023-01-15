1502d2008864ea1d1bf1bda9cb7f05181b5a9481
Rewarzhub <92559811+Rewarzhub@users.noreply.github.com>
2023-01-15 13:01:24 +0700

    Add files via upload

---
+++ discord_perms/config.lua
@@ -0,0 +1,10 @@
+Config = {
+	DiscordToken = "NjE0MjkzMzQ5MDAzNjkwMDA0.XV9YDA.BPU4lxtiqs0Z1fDxEsX1gJNEbqM",
+	GuildId = "614049751653941269",
+
+	-- Format: ["Role Nickname"] = "Role ID" You can get role id by doing \@RoleName
+	Roles = {
+		["whitelisted"] = "614295228731359282" ,-- This would be checked by doing exports.discord_perms:IsRolePresent(user, "TestRole")
+	
+	}
+}
